ecba496026241ef24a6ce00362cc0828dbbdcea0
youmari <youmari300@gmail.com>
2022-03-15 22:40:25 +0100

    Modify id to be automatically set up

--- item.rb
+++ item.rb
@@ -2,8 +2,8 @@ class Item
   attr_reader :genre, :author, :label, :source
   attr_accessor :publish_date
 
-  def initialize(id, date, archived: false)
-    @id = id || Random.rand(1..500)
+  def initialize(date, archived: false)
+    @id = Random.rand(1..500)
     @publish_date = date
     @archived = archived
   end
