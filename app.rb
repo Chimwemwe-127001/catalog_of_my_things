39a243d9c8aa7d8a32f8c8c4f27ec8d956fc183a
youmari <youmari300@gmail.com>
2022-03-16 16:20:07 +0100

    Implement user interface for games and authors

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require './classes/game'
+require './classes/author'
+
 class App
   def initialize
     @books = []
@@ -23,17 +26,68 @@ class App
 
   def list_all_music_album; end
 
-  def list_all_games; end
+  def list_all_games
+    puts 'There are no game please try to add one !' if @games.count.zero?
+    @games.each do |game|
+      puts "#{game.multiplayer}, Last played at: #{game.last_played_at}, Publish date: #{game.publish_date}"
+    end
+  end
 
   def list_all_genres; end
 
   def list_all_labels; end
 
-  def list_all_authors; end
+  def list_all_authors
+    @authors.each do |author|
+      puts "#{author.first_name} #{author.last_name}"
+    end
+  end
 
   def add_book; end
 
   def add_music_album; end
 
-  def add_game; end
+  def add_game
+    multiplayer, last_played_at = game_input
+    publish_date = publish_date_input
+
+    @games << Game.new(multiplayer, last_played_at, publish_date)
+    puts 'Game created successfully'
+    puts ''
+  end
+
+  def add_author
+    first_name, last_name = author_input
+
+    @authors << Author.new(first_name, last_name)
+    puts 'Author created successfully'
+    puts ''
+  end
+
+  private
+
+  def game_input
+    print 'Multiplayer: '
+    multiplayer = gets.chomp
+
+    print 'Last_played_at: '
+    last_played_at = gets.chomp.to_i
+
+    [multiplayer, last_played_at]
+  end
+
+  def publish_date_input
+    print 'Publish_date: '
+    gets.chomp.to_i
+  end
+
+  def author_input
+    print 'First name: '
+    first_name = gets.chomp
+
+    print 'Last name: '
+    last_name = gets.chomp
+
+    [first_name, last_name]
+  end
 end
