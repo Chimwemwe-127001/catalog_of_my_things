7e92a0fa2412200ae89f97b51a5bc8ed888c59fe
mongare <mongaresams@gmail.com>
2022-03-16 03:10:03 +0300

    Add the add_item function and association to item

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,8 +1,21 @@
+require_relative 'item'
+
 class Genre
     attr_accessor :name
+    attr_reader :items
+    
     def initialize(date, name)
         super(date)
         @name = name
         @items = []
+    end
+
+    def add_item(item)
+        @items = Item.new(item)
+        @items.genre = self
+    end
+
+    def music_album
+        @items.select {|song| song.genre == self}
     end
 end
