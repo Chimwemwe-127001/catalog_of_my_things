d0cff74b53b87158109f830673745343b137b7eb
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-03-17 06:00:07 +0200

    Create a schema.sql file with tables that will be analogical to the structure of classes created

--- database/schema.sql
+++ database/schema.sql
@@ -1,29 +1,25 @@
-DROP TABLE IF EXISTS games;
-
-CREATE TABLE games (
-    id INT PRIMARY KEY,
-    multiplayer VARCHAR(100),
-    last_played_at DATE NOT NULL,
-    genre_id INT NOT NULL,
-    lable_id INT NOT NULL,
-    source_id INT NOT NULL,
-    author_id INT NOT NULL,
-    publish_date DATE NOT NULL,
-    archived BOOLEAN NOT NULL,
-    FOREIGN KEY genre_id REFERENCES genre (id),
-    FOREIGN KEY lable_id REFERENCES lable (id),
-    FOREIGN KEY author_id REFERENCES author (id),
-    FOREIGN KEY source_id REFERENCES source (id)
+CREATE TABLE item (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
 );
 
-DROP TABLE IF EXISTS authors;
-
-CREATE TABLE authors (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL
+CREATE TABLE labels (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(100),
+    color VARCHAR(100),
+    PRIMARY KEY(id)
 );
 
-
-
-
+CREATE TABLE books (
+    id  INT,
+    title VARCHAR(100),
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    FOREIGN KEY(id) REFERENCES item(id)
+);
